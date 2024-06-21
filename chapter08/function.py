c62f4dab983184067650ace3c14fbc55cdbd74fb
guochao <463561248@qq.com>
2024-06-21 16:38:29 +0800

    函数

--- chapter08/function.py
+++ chapter08/function.py
@@ -15,3 +15,11 @@ get_user_name(name);
 print(get_user_name.__doc__)
 print(name)
 
+
+get_user_name(name= "saber");
+
+def get_dog_name(name="kaka"):
+    """显示小狗的名字"""
+    print(f"Dog name is {name}")
+
+    
